ee8902ce97e8e04ee304fc257e23508b894a5a1b
Ткаченко Алексей <tkacenk002@mail.ru>
2023-09-22 17:36:38 +0700

    добавил объявление функций

--- Client.h
+++ Client.h
@@ -49,3 +49,48 @@ struct Client {
 	vector<Deposit> Deposits;
 	vector<Account> Accounts;
 };
+
+//--------------------------Client-----------------------------------------------
+
+Client InitClient(string name, int age, string PhoneNumber);
+Client InitClientFromConsole();
+string GetName(Client client);
+string GetPhoneNumber(Client client);
+int GetAge(Client client);
+
+//---------------------------------------Account--------------------------------------
+
+void NewAccount(Client* client, double money);
+void InputNewAccountFromConsole(Client* client);
+int GetAccountId(Account account);
+double GetMoney(Account account);
+
+//------------------------Get-Credit-and-Deposit---------------------------------------
+int GetYear(Credit credit);
+int GetYear(Deposit deposit);
+
+double GetBody(Credit credit);
+double GetBody(Deposit deposit);
+
+double GetPercent(Credit credit);
+double GetPercent(Deposit deposit);
+
+//-------------------------------------Credit-----------------------------------------
+
+void NewCredit(Client* client, int years, double body, double percent, double contribution);
+void InputNewCreditFromConsole(Client* client);
+
+double GetContribution(Credit credit);
+double GetFinalContributionsPayments(Credit credit);
+
+//--------------------------------------Deposit------------------------------------------
+
+void NewDeposit(Client* client, int year, double body, double percent);
+void InputNewDepositFromConsole(Client* client);
+
+double GetFinalDepositAmount(Deposit deposit);
+
+//-----------------------------------------------Transaction-------------------------
+
+TransactionResult NewTransaction(Account* Account_1, Account* Account_2, double transactionMoney);
+void InputNewTransactionFromConsole(Account* Account_1, Account* Account_2);
